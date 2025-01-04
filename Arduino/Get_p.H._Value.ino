acbba7270e7dd1e5d11200a982236558a5ad07c0
Bhashkar Fulara <89032019+BhashkarFulara369@users.noreply.github.com>
2025-01-04 14:23:59 +0530

    Update Get_p.H._Value.ino

--- Arduino/Get_p.H._Value.ino
+++ Arduino/Get_p.H._Value.ino
@@ -1,15 +1,20 @@
-/* This code is written for getting the ph reading fromn the ph sensor and send it to the esp8266 (wifi microcontroller) using the arduino mega rx and tx pins and displayin it in the serial monitor and lcd (16*2) display. (If you use display than you can use )*/
-// Basic Code for diplaying 
+/*
+This code retrieves pH readings from a pH sensor and transmits them to an ESP8266 Wi-Fi microcontroller via the 
+Arduino Mega's RX and TX pins.
+The pH readings are displayed on both the serial monitor and an optional 16x2 LCD display.
+The LCD display is optional. If transmission of data from Arduino to ESP8266 is the sole requirement,
+the LCD display component can be omitted.
+*/
 #include <LiquidCrystal.h>;
 
-const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2,ct = 9;
+const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2,ct = 9; 
 LiquidCrystal mylcd(rs, en, d4, d5, d6, d7);
 
 
 // All Global Variables and their values 
 
-float calibration = 22.49; //change this value to calibrate
-const int analogInPin = A0; 
+float calibration = 22.49; //change this value to calibrate acc. to you
+const int analogInPin = A0; // Arduino Mega A0
 int sensorValue = 0; 
 unsigned long int avgValue; 
 float b;
@@ -57,8 +62,8 @@ for(int i=2;i<8;i++)
 avgValue+=buf[i];
 float pHVol=(float)avgValue*5.0/1024/6;
 float phValue = -5.70 * pHVol + calibration;
-String sv = String(phValue);
-Serial.println(sv);
+String sv = String(phValue);// Important Line
+Serial.println(sv); 
 
  //mylcd.setCursor(0,0);  
 // mylcd.print("pH_Value");
